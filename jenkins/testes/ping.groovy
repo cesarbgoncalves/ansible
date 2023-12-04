51b874299ab8138e08cd475c4218e007b9fcf91b
Cesar Barbosa Gonçalves <cesarbgoncalves@gmail.com>
2023-12-04 08:38:12 -0300

    tentativa-039

--- jenkins/testes/ping.groovy
+++ jenkins/testes/ping.groovy
@@ -48,48 +48,54 @@ pipeline {
         //         }
         //     }
         // }
-        stage('Ansible Plugin') {
+        // stage('Ansible Plugin') {
+        //     steps {
+        //         ansiColor('xterm') {
+        //         ansiblePlaybook(
+        //         playbook: 'playbooks/testes/ping.yaml',
+        //         inventory: 'hosts/proxmox.yaml',
+        //         credentialsId: 'ssh-root',
+        //         colorized: true)
+        //         }
+        //     }
+        // }
+        stage('Ansible Playbook') {
             steps {
-                ansiColor('xterm') {
-                ansiblePlaybook(
-                playbook: 'playbooks/testes/ping.yaml',
-                inventory: 'hosts/proxmox.yaml',
-                credentialsId: 'ssh-root',
-                colorized: true)
+                sh buildCommand(playbook: "playbooks/prod/delivery.apps/start.fe.yml")
                 }
             }
         }
     }
 }
 
-// def getLimit(Map map = [:]) {
-//     def targetList = []
-//     if (params.k3s) { targetList.push("k3s") }
-//     if (params.OPNSENSE) { targetList.push("opnsense") }
-//     if (params.NGINX_MANAGER) { targetList.push("nginx_manager") }
-//     if (params.MYSQL_PIHOLE) { targetList.push("mysql") }
+def getLimit(Map map = [:]) {
+    def targetList = []
+    if (params.k3s) { targetList.push("k3s") }
+    if (params.OPNSENSE) { targetList.push("opnsense") }
+    if (params.NGINX_MANAGER) { targetList.push("nginx_manager") }
+    if (params.MYSQL_PIHOLE) { targetList.push("mysql") }
 
-//     def limit = targetList.join(',')
-//     if (limit) limit = /--limit '${limit.toLowerCase()}'/
+    def limit = targetList.join(',')
+    if (limit) limit = /--limit '${limit.toLowerCase()}'/
 
-//     return limit
-// }
+    return limit
+}
 
-// def buildCommand(Map map = [:]) {
-//     def callback = map.useCallback ? "ANSIBLE_STDOUT_CALLBACK=diy" : ""
-//     def tags = map.tags ? map.tags.collect{ "--tags $it" }.join(" ") : ""
-//     def extra = map.extra ? map.extra.collect{entry -> "--extra-vars '${entry.key}=${entry.value}'"}.join(" ") : ""
-//     def verbose = (map.verbose != null ? map.verbose : true) ? "--verbose" : ""
-//     def list_hosts = (map.list_hosts != null ? map.list_hosts : false) ? "--list-hosts" : ""
+def buildCommand(Map map = [:]) {
+    def callback = map.useCallback ? "ANSIBLE_STDOUT_CALLBACK=diy" : ""
+    def tags = map.tags ? map.tags.collect{ "--tags $it" }.join(" ") : ""
+    def extra = map.extra ? map.extra.collect{entry -> "--extra-vars '${entry.key}=${entry.value}'"}.join(" ") : ""
+    def verbose = (map.verbose != null ? map.verbose : true) ? "--verbose" : ""
+    def list_hosts = (map.list_hosts != null ? map.list_hosts : false) ? "--list-hosts" : ""
 
-//     col = COLUNA.split(',').first().replaceAll(/(\w+)(\d+)/,/$1 $2/).toUpperCase()
-//     def limit = getLimit(map)
+    col = COLUNA.split(',').first().replaceAll(/(\w+)(\d+)/,/$1 $2/).toUpperCase()
+    def limit = getLimit(map)
 
-//     return """
-//         $callback ansible-playbook $verbose ${map.playbook} $list_hosts $tags \
-//         -i hosts/gt.${ENV}.yml -i hosts/tb.${ENV}.yml $limit $extra \
-//         --user=$SSH_CREDENTIAL_USR --private-key=$SSH_CREDENTIAL \
-//         -e 'COLUMN="$col"' \
-//         -e base_path=\$PWD
-//     """
-// }
+    return """
+        $callback ansible-playbook $verbose ${map.playbook} $list_hosts $tags \
+        -i hosts/gt.${ENV}.yml -i hosts/tb.${ENV}.yml $limit $extra \
+        --user=$SSH_CREDENTIAL_USR --private-key=$SSH_CREDENTIAL \
+        -e 'COLUMN="$col"' \
+        -e base_path=\$PWD
+    """
+}
