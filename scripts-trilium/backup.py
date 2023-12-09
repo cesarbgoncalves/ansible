062ba3a5db6d44b1a30cee342cb1031ef475f317
Cesar Barbosa Gonçalves <cesarbgoncalves@gmail.com>
2023-12-09 11:18:18 -0300

    vai

--- scripts-trilium/backup.py
+++ scripts-trilium/backup.py
@@ -10,7 +10,7 @@ agora = datetime.datetime.now()
 
 data_formatada = agora.strftime("%d/%m/%Y %H:%M:%S") 
 
-data_formatadaID = agora.strftime("%Y%m%d")
+data_formatadaID = agora.strftime("%Y-%m-%d")
 
 res = res = ea.backup(data_formatadaID)
 
