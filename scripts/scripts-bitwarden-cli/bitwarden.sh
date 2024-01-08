001efa81803a556f9f787a191efa1e07f6ce81aa
Cesar Barbosa Gonçalves <cesarbgoncalves@gmail.com>
2024-01-08 18:54:29 -0300

    script finalizado

--- scripts/scripts-bitwarden-cli/bitwarden.sh
+++ scripts/scripts-bitwarden-cli/bitwarden.sh
@@ -15,5 +15,8 @@ lista=$(bw list items --session ${BW_SESSION} --folderid 29752335-d158-4a48-b036
 
 
 for item in ${lista}; do
-    printf "$item"
+# bw get item $item | jq '.login.password="$NOVA_SENHA"' | bw encode | bw edit item $item
+echo "agora vai o item $item usando a senha "$NOVA_SENHA \n"
 done
+
+bw sync
