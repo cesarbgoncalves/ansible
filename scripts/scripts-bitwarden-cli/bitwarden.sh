56cfea00e1f6008400fef5ac22ca7e002fad10d1
Cesar Barbosa Gonçalves <cesarbgoncalves@gmail.com>
2024-01-08 18:33:20 -0300

    bug

--- scripts/scripts-bitwarden-cli/bitwarden.sh
+++ scripts/scripts-bitwarden-cli/bitwarden.sh
@@ -2,7 +2,7 @@
 export BW_CLIENTID=$1
 export BW_CLIENTSECRET=$2
 export BW_PASSWORD=$3
-bw config server https://bitwarden.cesarbgoncalves.com.br
+bw config server https://bitwarden.cesarbgoncalves.com.br > /dev/null 2>&1
 bw login --apikey > /dev/null 2>&1
 bw unlock --passwordenv BW_PASSWORD > /dev/null 2>&1
 
