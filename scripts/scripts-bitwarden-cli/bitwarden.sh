aa2d43dbace330ac01ee9cc8853ab6db813fd6fe
Cesar Barbosa Gonçalves <cesarbgoncalves@gmail.com>
2024-01-08 19:40:34 -0300

    bug

--- scripts/scripts-bitwarden-cli/bitwarden.sh
+++ scripts/scripts-bitwarden-cli/bitwarden.sh
@@ -15,7 +15,7 @@ lista=$(bw list items --session ${BW_SESSION} --folderid 29752335-d158-4a48-b036
 
 
 for item in ${lista}; do
-    bw get item $item | jq '.login.password="$NOVA_SENHA"' | bw encode | bw edit item $item
+    bw get item $item --session ${BW_SESSION}| jq '.login.password="$NOVA_SENHA"' | bw encode | bw edit item $item
 done
 
 bw sync
