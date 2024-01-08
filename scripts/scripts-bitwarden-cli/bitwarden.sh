1fd28052115d5251fb8f65b83b57cc879864162a
Cesar Barbosa Gonçalves <cesarbgoncalves@gmail.com>
2024-01-08 18:30:36 -0300

    bug

--- scripts/scripts-bitwarden-cli/bitwarden.sh
+++ scripts/scripts-bitwarden-cli/bitwarden.sh
@@ -5,3 +5,6 @@ export BW_PASSWORD=$3
 bw config server https://bitwarden.cesarbgoncalves.com.br
 bw login --apikey
 bw unlock --passwordenv BW_PASSWORD > /dev/null 2>&1
+
+lista=[]
+lista=`bw list items --folderid 29752335-d158-4a48-b036-f206289ce954 | jq -r '.[].id'`
