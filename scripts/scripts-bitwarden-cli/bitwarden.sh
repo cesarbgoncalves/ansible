700373d18379e657f0f54c51aa559841c45a607a
Cesar Barbosa Gonçalves <cesarbgoncalves@gmail.com>
2024-01-08 18:44:30 -0300

    bug

--- scripts/scripts-bitwarden-cli/bitwarden.sh
+++ scripts/scripts-bitwarden-cli/bitwarden.sh
@@ -5,8 +5,8 @@ export BW_PASSWORD=$3
 bw config server https://bitwarden.cesarbgoncalves.com.br > /dev/null 2>&1
 bw login --apikey > /dev/null 2>&1
 bw unlock --passwordenv BW_PASSWORD > /dev/null 2>&1
+BW_SESSION=$(bw unlock --passwordenv BW_PASSWORD | grep export | awk -F\" '{print $2}')
 
-lista="ola mundo"
-lista=$(bw list items --folderid 29752335-d158-4a48-b036-f206289ce954 | jq -r '.[].id')
+lista=$(bw list items --session ${BW_SESSION} --folderid 29752335-d158-4a48-b036-f206289ce954 | jq -r '.[].id')
 
 printf "$lista"
