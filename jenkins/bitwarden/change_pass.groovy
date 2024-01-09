f25c3ab643fd8f64b53b84ccf4fa2b330eb9ad09
Cesar Barbosa Gonçalves <cesarbgoncalves@gmail.com>
2024-01-09 14:11:39 -0300

    teste

--- jenkins/bitwarden/change_pass.groovy
+++ jenkins/bitwarden/change_pass.groovy
@@ -39,7 +39,7 @@ pipeline {
                     sh(script: """
                     bw config server https://bitwarden.cesarbgoncalves.com.br --quiet
                     bw login --apikey
-                    echo 'bw unlock --passwordenv BW_PASSWORD --quiet'
+                    echo 'oi'
                     bw list items --folderid '29752335-d158-4a48-b036-f206289ce954' | jq -r '.[].name'
                     """)
                 }
