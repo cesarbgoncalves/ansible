ab19670fe5918795d907ab24d915fc3cb1fdf5e8
Cesar Barbosa Gonçalves <cesarbgoncalves@gmail.com>
2024-01-09 13:39:35 -0300

    teste

--- jenkins/bitwarden/change_pass.groovy
+++ jenkins/bitwarden/change_pass.groovy
@@ -38,10 +38,11 @@ pipeline {
                 script {
                     sh "config server https://bitwarden.cesarbgoncalves.com.br --quiet"
                     sh "bw login --apikey --quiet"
-                    def BW_SESSION = sh(returnStdout: true, script: """
-                        bw unlock --passwordenv BW_PASSWORD | grep export | awk -F"'" '{print $2}'
-                    """)
-                    sh "bw list items --session $BW_SESSION --folderid '29752335-d158-4a48-b036-f206289ce954' | jq -r '.[].name'"
+                    def comando = """
+                    bw unlock --passwordenv BW_PASSWORD | grep export | awk -F"'" \'{print $2}\'
+                    """
+                    def BW_SESSION = sh(returnStdout: true, script:"$comando")
+                    sh "bw list items --session ${BW_SESSION} --folderid '29752335-d158-4a48-b036-f206289ce954' | jq -r '.[].name'"
                 }
             }
             // steps {
