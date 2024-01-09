0a3a41f50d19d44ba51b5eac509eb7f80e4be167
Cesar Barbosa Gonçalves <cesarbgoncalves@gmail.com>
2024-01-09 15:18:42 -0300

    teste

--- jenkins/bitwarden/change_pass.groovy
+++ jenkins/bitwarden/change_pass.groovy
@@ -38,7 +38,7 @@ pipeline {
                 script {
                     sh(script: """
                     bw config server https://bitwarden.cesarbgoncalves.com.br --quiet
-                    bw login --apikey -v
+                    bw login --apikey
                     export BW_SESSION=`bw list items --folderid '29752335-d158-4a48-b036-f206289ce954' | jq -r '.[].name'`
                     """)
                 }
